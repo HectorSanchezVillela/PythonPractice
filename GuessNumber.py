0d17db72eab21c3fb2f38d9bbd751e6e3ccf52c5
Hector <isc.sanchezvillela@gmail.com>
2022-08-20 22:54:08 -0600

    Cambios a GuessNumber

--- GuessNumber.py
+++ GuessNumber.py
@@ -1,1 +1,16 @@
-import math
+import random
+import discord
+def run():
+    x=random.randint(1,99)
+    n=int(input("!!!Guess the number! (1 to 99): "))
+    while n!=x:
+        if n>x:
+            n=int(input("Too High! Aim lower: "))
+        else:
+            n=int(input("Too Low! Aim higher: "))
+    print("You guessed it! The number is: "+str(x))
+
+
+
+if __name__ == '__main__':
+    run()
